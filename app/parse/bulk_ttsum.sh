559e0016b845e5f27507bd57b741c220ee6a9c99
Colin Drewes <drewes@cs.stanford.edu>
2024-04-05 03:49:09 +0000

    test PCIe speeds script

--- app/parse/bulk_ttsum.sh
+++ app/parse/bulk_ttsum.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-for f in perf_write_*; do python3 ttsum.py -f "write request" $f; done
+for f in perf_write_*; do echo $f; python3 ttsum.py -f "write request" $f; done
--- app/src/perf_write.cc
+++ app/src/perf_write.cc
@@ -45,10 +45,15 @@ int main() {
     char pattern[5] = "\xDE\xAD\xBE\xEF";
 
     for (int i = 0; i < (HOMA_MAX_MESSAGE_LENGTH/64)-1; ++i) {
-        memset(pattern, i+2, 4);
+	memset(pattern, 0xFF, 4);
     	for (int j = 0; j < 64/4; ++j) {
-        	memcpy((((char*) h2c_msgbuff_map)) + (j*4) + (i*64), pattern, 4);
-    	}
+       		 memcpy((((char*) h2c_msgbuff_map)) + (j*4) + (i*64), pattern, 4);
+	}	
+
+        //memset(pattern, i+2, 4);
+    	//for (int j = 0; j < 64/4; ++j) {
+        //	memcpy((((char*) h2c_msgbuff_map)) + (j*4) + (i*64), pattern, 4);
+    	//}
     }
 
     uint32_t retoff = 0; // Lte 12 bits used
@@ -61,20 +66,18 @@ int main() {
     msghdr_send_in.id        = 0;
     msghdr_send_in.cc        = 0;
 
-    // char * poll = ((char *) c2h_metadata_map) + ((retoff) * 64) + 10;
-    for (int i = 0; i < 10; ++i) {
-
+    for (int i = 0; i < 200; ++i) {
     	char thread_name[50];
     	snprintf(thread_name, sizeof(thread_name), "main");
     	time_trace::thread_buffer thread_buffer(thread_name);
 
-    	for (int j = 0; j < 32; ++j) {
-    		// uint32_t size   = 16384; // Lte 20 bits used
-    		uint32_t size   = 1024 * (i+1); // Lte 20 bits used
+    	for (int j = 0; j < 16; ++j) {
+    		uint32_t size   = 1000 * (i+1); // Lte 20 bits used
     		msghdr_send_in.metadata  = (size << 12) | retoff;
 
 		volatile char * poll = ((volatile char *) c2h_msgbuff_map) + size - 1;
     		*poll = 0;
+		_mm_mfence();
 
 		__m512i ymm0;
 		ymm0 = _mm512_load_si512(reinterpret_cast<__m512i*>(&msghdr_send_in));
@@ -84,32 +87,13 @@ int main() {
 		_mm512_store_si512(reinterpret_cast<__m512i*>(((char *) h2c_metadata_map)), ymm0);
 		while(*poll == 0);
 		tt(rdtsc(), "write response", 0, 0, 0, 0);
+		printf("GOOD %d\n", i);
 	}
 
 	char file_name[50];
-	snprintf(file_name, 50, "parse/perf_write_%d.tt\0", i);
+	snprintf(file_name, 50, "parse/perf_write_%03d.tt\0", i);
     	time_trace::print_to_file(file_name);
     } 
-
-    // std::this_thread::sleep_for(std::chrono::milliseconds(10));
-    // printf("Data buffer in \n");
-    // for (int i = 0; i < 256; ++i) {
-    // 	printf("Chunk %d: ", i);
-    // 	for (int j = 0; j < 64; ++j) printf("%02hhX", *(((unsigned char *) h2c_msgbuff_map) + j + (i*64)));
-    // 	printf("\n");
-    // }
-
-
-    // printf("Data buffer out\n");
-    // for (int i = 0; i < 256; ++i) {
-    // 	printf("Chunk %d: ", i);
-    // 	for (int j = 0; j < 64; ++j) printf("%02hhX", *(((unsigned char *) c2h_msgbuff_map) + j + (i*64)));
-    // 	printf("\n");
-    // }
-
-    // printf("memcmp: %d\n", memcmp(c2h_msgbuff_map, h2c_msgbuff_map, size-1));
-
-
 
     munmap(h2c_metadata_map, 16384);
     munmap(c2h_metadata_map, 16384);
