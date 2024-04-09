d34a962f0b0e9232f5924555dd00f35ade0ead3f
Colin Drewes <drewes@cs.stanford.edu>
2024-04-09 16:40:39 -0700

    Simulator: Remove network ingress queue

--- model/Network.py
+++ model/Network.py
@@ -6,44 +6,39 @@ class Network():
     # This routes data from ingress to egress
     def __init__(self, channels, config):
         # Packets coming into the switch
-        self.ingress = [[] for i in range(channels)]
+        # self.ingress = [[] for i in range(channels)]
 
         # Packets going out of the switch
-        self.egress  = [[] for i in range(channels)]
+        self.egress  = []
 
         self.config = config
         self.channels = channels
 
         self.time = 0
 
-    def peek(self, id):
-        return (len(self.ingress[id]) < self.config['bufferDepth'])
-
     # Outgoing packets towards the network
-    def write(self, dest, message):
+    def write(self, dest, packet):
         # Add to the respective buffer the time of insertion and the message
-        if (len(self.ingress[dest]) < self.config['bufferDepth']):
-            self.ingress[dest].append((self.time, message))
+        if (len(self.egress) < self.config['bufferDepth']):
+            self.egress.append((self.time, dest, packet))
             return True
 
         return False 
 
     # Incoming packets coming from the network
     def read(self, dest):
+        match = None
         # Are there any pending elements on the network
-        if (self.egress[dest]):
-            if (self.egress[dest][0][0] + int(self.config['intrinsicDelay']/2) < self.time):
-                return self.egress[dest].pop(0)[1]
+        for buffered in self.egress:
+            if buffered[1] == dest and (buffered[0] + self.config['delay']) < self.time:
+                match = buffered
+                break
+
+        if match != None:
+            self.egress.remove(match)
+            return match[2]
+
         return None
 
     def tick(self):
-        random.shuffle(self.ingress)
-        for buffer in self.ingress:
-            if (len(buffer)):
-                head = buffer[-1]
-                dest = self.egress[head[1].message.dest] 
-                if (len(dest) < self.config['bufferDepth']):
-                    buffer.remove(head)
-                    dest.append(head)
-
         self.time += 1
